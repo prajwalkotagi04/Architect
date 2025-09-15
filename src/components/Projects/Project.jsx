47ce2f4229df91a6348858d0e940d1883c782f44
prajwal <prajwalkotagi06@gmail.com>
2025-09-15 23:09:02 +0530

    update project

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -5,16 +5,16 @@ export default function Project() {
   return (
     <>
         <div className='grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-10 px-5 md:px-20 py-10 md:py-24'>
-            <img src={projects1} alt="" className='w-[100%] h-40 md:h-90 object-cover'/>
-            <img src={projects2} alt=""  className='w-[100%] h-40 md:h-90 object-cover'/>
-            <img src={projects3} alt=""  className='w-[100%] h-40 md:h-90 object-cover'/>
-            <img src={projects4} alt=""  className='w-[100%] h-40 md:h-90 object-cover'/>
-            <img src={projects5} alt=""  className='w-[100%] h-40 md:h-90 object-cover'/>
-            <img src={projects6} alt=""  className='w-[100%] h-40 md:h-90 object-cover'/>
-            <img src={projects7} alt=""  className='w-[100%] h-40 md:h-90 object-cover'/>
-            <img src={projects8} alt=""  className='w-[100%] h-40 md:h-90 object-cover'/>
-            <img src={projects9} alt=""  className='w-[100%] h-40 md:h-90 object-cover'/>
-            <img src={projects10} alt="" className='w-[100%] h-40 md:h-90 object-cover' />
+            <img src={projects1} alt="projects" className='w-[100%] h-40 md:h-90 object-cover'/>
+            <img src={projects2} alt="projects"  className='w-[100%] h-40 md:h-90 object-cover'/>
+            <img src={projects3} alt="projects"  className='w-[100%] h-40 md:h-90 object-cover'/>
+            <img src={projects4} alt="projects"  className='w-[100%] h-40 md:h-90 object-cover'/>
+            <img src={projects5} alt="projects"  className='w-[100%] h-40 md:h-90 object-cover'/>
+            <img src={projects6} alt="projects"  className='w-[100%] h-40 md:h-90 object-cover'/>
+            <img src={projects7} alt="projects"  className='w-[100%] h-40 md:h-90 object-cover'/>
+            <img src={projects8} alt="projects"  className='w-[100%] h-40 md:h-90 object-cover'/>
+            <img src={projects9} alt="projects"  className='w-[100%] h-40 md:h-90 object-cover'/>
+            <img src={projects10} alt="projects" className='w-[100%] h-40 md:h-90 object-cover' />
 
         </div>
     </>
